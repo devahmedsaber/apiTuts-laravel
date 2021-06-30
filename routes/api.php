7a0819e5d4be5baba8d6968a523a8e8e52e6820c
Ahmed Saber <devahmedsaber@gmail.com>
2021-06-30 17:21:24 +0200

    Categories Model & Controller and Get Main Categories Api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// All Routes - API Here Not Authenticated
+Route::group(['middleware' => 'api', 'namespace' => 'Api'], function (){
+    Route::post('get-main-categories', 'CategoriesController@index');
+});
